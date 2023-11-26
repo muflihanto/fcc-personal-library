d17b9f8aa58c83eb47c646778c380e89f340b982
muflihanto <muflihanto@gmail.com>
2023-11-26 15:20:57 +0700

    add initial connection to mongodb

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,60 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-module.exports = function (app) {
+/**
+ * @param {import('express').Express} app - Express app instance
+ * @param {import('mongodb').Db} database - Database instance
+ */
+module.exports = function (app, database) {
+  const collection = database.collection("books");
 
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(function (req, res) {
+      collection
+        .find({})
+        .toArray()
+        .then((docs) => {
+          res.json(docs);
+        })
+        .catch((_) => {
+          res.status(404).json({ result: "not found" });
+        });
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       //if successful response will be 'complete delete successful'
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function (req, res) {
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
     })
-    
-    .delete(function(req, res){
+
+    .delete(function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
-  
 };
